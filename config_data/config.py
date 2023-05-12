3c6ef01a8011bfc6bed926cba50273cf94df0770
Aleksandr Gladkikh <20492198+galexla@users.noreply.github.com>
2023-05-12 15:14:41 +0400

    Add env variables, make debugging optional

--- config_data/config.py
+++ config_data/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import find_dotenv, load_dotenv
+from distutils.util import strtobool
 
 if not find_dotenv():
     exit('No environment variables found, .env file does not exist')
@@ -14,6 +15,14 @@ REDIS_HOST = os.getenv('REDIS_HOST')
 REDIS_PORT = os.getenv('REDIS_PORT')
 REDIS_DB = os.getenv('REDIS_DB')
 REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
+
+POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+
+DEBUG = strtobool(os.getenv('DEBUG', 'False'))
 
 # TODO: remove summary command
 DEFAULT_COMMANDS = (
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import debugpy
 from telebot.custom_filters import StateFilter
 
+from config_data import config
 import handlers  # noqa
 from loader import bot
 from utils.set_bot_commands import set_default_commands
 
 if __name__ == '__main__':
-    # TODO: remove
-    debugpy.listen(('0.0.0.0', 5678))
+    if config.DEBUG:
+        import debugpy
+        debugpy.listen(('0.0.0.0', 5678))
 
     bot.add_custom_filter(StateFilter(bot))
     set_default_commands(bot)

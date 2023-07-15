490e3c1b7053a013bce9342b7c6f06f9eeb8242f
Aleksandr Gladkikh <20492198+galexla@users.noreply.github.com>
2023-07-16 01:15:10 +0400

    Fix tests for important_news and important_words

--- tests/utils/news/test_important_words.py
+++ tests/utils/news/test_important_words.py
@@ -27,8 +27,8 @@ def test_get_words_importance():
     Tests _get_words_importance() function
     """
     all_words = ['hi', 'hi', 'Xi', 'Xi', 'Xi', 'Qing', 'Qing', 'water']
-    expected = {'Qing': 17.531030639740447, 'Xi': 14.79406504557424,
-                'hi': 11.287712379549449, 'water': 8.560881162516957}
+    expected = {'hi': 7.8244459312774595, 'Xi': 10.254499674775916,
+                'Qing': 12.151589740237444, 'water': 5.93659514377329}
     actual = _get_words_importance(all_words)
     assert actual == expected
 
@@ -38,7 +38,7 @@ def test_get_words_importance():
     assert actual == expected
 
     all_words = ['water']
-    expected = {'water': 11.560881162516957}
+    expected = {'water': 8.013723128149687}
     actual = _get_words_importance(all_words)
     assert actual == expected
 
@@ -48,8 +48,8 @@ def test_get_important_words():
     Tests get_important_words() function
     """
     text = 'How long is the present? The answer, Cornell researchers suggest in a new study, depends on your heart. The researchers discovered that our moment-to-moment perception of time is not constant and can expand or contract with each heartbeat. According to Adam K. Anderson, a professor in the Depa.'
-    excepted = {'depa': 18.671542913362718, 'heartbeat': 12.06120384974077, 'cornell': 11.994930630321603, 'researchers': 10.673002535434241, 'perception': 10.371235735111734,
-                'expand': 9.802285552379209, 'anderson': 9.770436686339867, 'depends': 9.204499011300468, 'constant': 9.170848621858552, 'adam': 9.07048166332878}
+    excepted = {'depa': 12.942129722100923, 'heartbeat': 8.360423415466789, 'cornell': 8.314497317656889, 'researchers': 7.398573928655951, 'perception': 7.189547524600555,
+                'expand': 6.795545968061798, 'anderson': 6.773494988107069, 'depends': 6.381766104950554, 'constant': 6.35848136394146, 'adam': 6.289037065455538}
     actual = get_important_words([text])
     for word, importance in excepted.items():
         assert actual[word] == importance
